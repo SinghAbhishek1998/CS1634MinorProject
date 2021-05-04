21f22b5dc577fabba4d57cc1a7ebc95168917f70
Abhishek Singh <abhisheksja1998@gmail.com>
2021-05-04 08:45:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,4 @@ def index():
 
 # start flask app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=os.getenv('PORT', 5000))
